d926ecc33d6796e5661248db27667c73829e0ada
baronowen <owenashby@hotmail.nl>
2019-02-24 13:16:47 +0100

    added discord bot
    Still requires implementation of converter logic

---
+++ ConverterBotDiscord/MyCommands.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Interactivity;
+using Engine;
+
+namespace ConverterBotDiscord
+{
+    public class MyCommands
+    {
+        [Command("hi")]
+        public async Task Hi(CommandContext ctx)
+        {
+            await ctx.RespondAsync($"👋 Hi, {ctx.User.Mention}!");
+            var interactivity = ctx.Client.GetInteractivityModule();
+            var msg = await interactivity.WaitForMessageAsync(xm => xm.Author.Id == ctx.User.Id && xm.Content.ToLower() == "how are you?", TimeSpan.FromMinutes(1));
+            if (msg != null)
+                await ctx.RespondAsync($"I'm fine, thank you!");
+        }
+
+        [Command("random")]
+        public async Task Random(CommandContext ctx, int min, int max)
+        {
+            var rnd = new Random();
+            await ctx.RespondAsync($"🎲 Your random number is: {rnd.Next(min, max)}");
+        }
+
+        [Command("convert")]
+        public async Task StartConvert(CommandContext ctx)
+        {
+            await ctx.RespondAsync($"Starting convert program.");
+            
+        }
+    }
+}
---
+++ ConverterBotDiscord/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.Interactivity;
+
+namespace ConverterBotDiscord
+{
+    class Program
+    {
+        static DiscordClient discord;
+        static CommandsNextModule commands;
+        static InteractivityModule interactivity;
+
+        static void Main(string[] args)
+        {
+            MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            discord = new DiscordClient(new DiscordConfiguration
+            {
+                Token = "NTQ5MTYwNTI5MzYxNTAyMjE5.D1P1aw.DU1ycDcsu6KGaW9RsR4rWSMdCFg",
+                TokenType = TokenType.Bot,
+                UseInternalLogHandler = true,
+                LogLevel = LogLevel.Debug
+
+            });
+
+            discord.MessageCreated += async e =>
+            {
+                if (e.Message.Content.ToLower().StartsWith("ping"))
+                    await e.Message.RespondAsync("pong!");
+            };
+
+            commands = discord.UseCommandsNext(new CommandsNextConfiguration
+            {
+                StringPrefix = "!"
+            });
+
+            commands.RegisterCommands<MyCommands>();
+            interactivity = discord.UseInteractivity(new InteractivityConfiguration
+            {
+
+            });
+            
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
